9eec589e42b9795bc597fc338f6527f83e3bb496
ZeroBone <zerobone21@gmail.com>
2020-07-30 18:26:35 +0200

    New chrem algoritm for non-coprime divisors

--- chrem.py
+++ chrem.py
@@ -7,31 +7,90 @@
 
 from euklidian import gcd, extended_gcd
 
-class CongruenceModulo:
-    def __init__(self, x, modulo):
-        self.x = x
-        self.modulo = modulo
+def chrem_relativelyPrime(congruence1, congruence2):
+    (a1, n1) = congruence1
+    (a2, n2) = congruence2
 
-    def normalize(self):
-        self.x = self.x % self.modulo # Works also for negative numbers
+    moduloGcd = extended_gcd(n2, n1)
+
+    assert moduloGcd.gcd == 1
+
+    solution = n2 * a1 * moduloGcd.u + n1 * a2 * moduloGcd.v
+
+    modulo = n1 * n2
+
+    return (solution % modulo, modulo)
 
 def chrem(congruence1, congruence2):
-    n1 = congruence1.modulo
-    n2 = congruence2.modulo
-    # x = n2 * a1 * u + n1 * a2 * v
-    #     mod n1        mod n2
-    nsgcd = gcd(n1, n2)
+    (a1, n1) = congruence1
+    (a2, n2) = congruence2
 
-    n1 = n1 // nsgcd
-    n2 = n2 // nsgcd
+    moduloGcd = gcd(n1, n2)
 
-    erw_result = extended_gcd(n2, n1)
+    if moduloGcd == 1:
+        return chrem_relativelyPrime(congruence1, congruence2)
 
-    # print(erw_result.u, erw_result.v)
+    n1 = n1 // moduloGcd
+    n2 = n2 // moduloGcd
 
-    solution = n2 * congruence1.x * erw_result.u + n1 * congruence2.x * erw_result.v
+    additionalCongruence = None
+    
+    if n1 % moduloGcd == 0:
+        assert n2 % moduloGcd != 0
+        # n2 % moduloGcd != 0 because otherwise the gcd would be incorrect
+        # that means the second congruence must be added
+        if moduloGcd % n2 == 0:
+            assert moduloGcd > n2
+            n2 = moduloGcd
+        else:
+            # if x = a2 mod gcd(n1, n2) doesn't already imply x = a2 mod n2/gcd(n1,n2)
+            assert gcd(moduloGcd, n2) == 1
+            additionalCongruence = (a2 % moduloGcd, moduloGcd)
+    elif n2 % moduloGcd == 0:
+        assert n1 % moduloGcd != 0
+        # n1 % moduloGcd != 0 because otherwise the gcd would be incorrect
+        # add first congruence
+        if moduloGcd % n1 == 0:
+            assert moduloGcd > n1
+            n1 = moduloGcd
+        else:
+            # if x = a1 mod gcd(n1, n2) doesn't already imply x = a1 mod n1/gcd(n1,n2)
+            assert gcd(moduloGcd, n1) == 1
+            additionalCongruence = (a1 % moduloGcd, moduloGcd)
+    else:
+        # the new n1 and n2 cannot be further divided by the gcd
+        # so we must add the missing terms
+        if a1 % moduloGcd != a2 % moduloGcd:
+            # the two additional and mandatory congruences are not equivalent
+            # therefore, the congruence system cannot be solved
+            return None
+        # a1 = a2 mod gcd(n1, n2)
+        # check that the new congruence will not imply some already existing one
+        if moduloGcd % n1 == 0:
+            assert moduloGcd > n1
+            n1 = moduloGcd
+        elif moduloGcd % n2 == 0:
+            assert moduloGcd > n2
+            n2 = moduloGcd
+        else: # if moduloGcd % n1 != 0 and moduloGcd % n2 != 0:
+            # if x = a1 (=a2) mod gcd(n1, n2) doesn't already
+            # imply x = a1 mod n1/gcd(n1,n2) or x = a2 mod n2/gcd(n1,n2)
+            assert gcd(moduloGcd, n1) == 1
+            assert gcd(moduloGcd, n2) == 1
+            additionalCongruence = (a1 % moduloGcd, moduloGcd)
 
-    return CongruenceModulo(solution, n1 * n2 * nsgcd)
+    a1 = a1 % n1
+    a2 = a2 % n2
+    
+    print([(a1, n1), (a2, n2), additionalCongruence])
+    
+    # the solution of the two first congruences
+    solution = chrem_relativelyPrime((a1, n1), (a2, n2))
+
+    if additionalCongruence != None:
+        solution = chrem_relativelyPrime(solution, additionalCongruence)
+
+    return solution
 
 def chrem_multiple(congruences):
     if len(congruences) == 0:
@@ -54,12 +113,14 @@ if __name__ == "__main__":
     for i in range(equations):
         print("")
         print("> Congruence " + str(i + 1) + ":")
-        x = int(input("Enter x: "))
-        modulo = int(input("Enter modulo: "))
-        congruences.append(CongruenceModulo(x, modulo))
+        x = int(input("x is congruent to: "))
+        modulo = int(input("... modulo: "))
+        congruences.append((x, modulo))
 
     solution = chrem_multiple(congruences)
-    solution.normalize()
 
     print("")
-    print("> Solution: " + str(solution.x) + " + " + str(solution.modulo) + "Z")
+    if (solution == None):
+        print("> This congruence system is unsolvable.")
+    else:
+        print("> L = " + str(solution[0]) + " + " + str(solution[1]) + "Z")
--- euklidian.py
+++ euklidian.py
@@ -5,22 +5,26 @@
     For more information see the LICENCE file.
 """
 
-def gcd(a, b):
-    while True:
-        if a == 0:
-            return b
-        b = b % a
-        if b == 0:
-            return a
-        a = a % b
-
-# Extended Euklidian Algorithm
+# Class to represent extended euklidian algorithm result
 class GCD_Result:
     def __init__(self, gcd, u, v):
         self.gcd = gcd
+        # u and v are the linear combination coefficients
         self.u = u
         self.v = v
+    def __str__(self):
+        return str(self.gcd) + " = " + str(self.u) + " * a + " + str(self.v) + " * b"
 
+def gcd(a, b):
+    while True:
+        if b == 0:
+            return a
+        a = a % b
+        if a == 0:
+            return b
+        b = b % a
+
+"""
 def extended_gcd(a, b):
     if a == 0:
         return GCD_Result(b, 0, 1)
@@ -32,3 +36,35 @@ def extended_gcd(a, b):
     result.v = u
 
     return result
+"""
+
+def extended_gcd(a, b):
+    if a == 0:
+        return GCD_Result(b, 0, 1)
+
+    if b == 0:
+        return GCD_Result(a, 1, 0)
+
+    unPrev = 1
+    vnPrev = 0
+    unCur = 0
+    vnCur = 1
+
+    while True:
+        bn = a // b
+        newB = a % b
+        a = b
+        b = newB
+
+        if b == 0:
+            return GCD_Result(a, unCur, vnCur)
+
+        # Update coefficients
+        unNew = unPrev - bn * unCur
+        vnNew = vnPrev - bn * vnCur
+
+        # Shift coefficients
+        unPrev = unCur
+        vnPrev = vnCur
+        unCur = unNew
+        vnCur = vnNew
